730fdc06d0f214abf23848f2fb573ba5d90b77bf
Cacheira14 <cacheira14@gmail.com>
2023-09-12 00:02:17 +0100

    Tutorial 4 (2:06:22)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <form action="/p" enctype="multipart/form-data" method="POST">
+            @csrf
             <div class="row">
                 <div class="col-8 offset-2">
 
@@ -13,22 +14,18 @@
                     <div class="row mb-3">
                         <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
 
-                        <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
-                            caption="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+                        <input id="caption" type="text" class="form-control" name="caption"
+                            @error('caption') is-invalid @enderror" value="{{ old('caption') }}" autofocus required>
 
                         @error('caption')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                            <strong>{{ $message }}</strong>
                         @enderror
                     </div>
                     <div class="row">
                         <label for="image" class="col-md-4 col-form-label">Image</label>
-                        <input type="file" name="image" id="image" class="form-control-file">
+                        <input type="file" name="image" id="image" class="form-control-file" required>
                         @error('image')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                            <strong>{{ $message }}</strong>
                         @enderror
                     </div>
 
